1d1e225a4920d169d9b258f7fa41802dc19aa105
atul kumar <atulkumar79@gmail.com>
2020-08-07 11:56:11 -0500

    hide the right cog selection

--- src/pages/Layout/Extra/FixedPlugin.vue
+++ src/pages/Layout/Extra/FixedPlugin.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed-plugin" v-click-outside="closeDropDown">
+  <div class="fixed-plugin" v-click-outside="closeDropDown" style="display:none;">
     <div class="dropdown show-dropdown" :class="{ show: isOpen }">
       <a data-toggle="dropdown">
         <i class="fa fa-cog fa-2x" @click="toggleDropDown"> </i>
