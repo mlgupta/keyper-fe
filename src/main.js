eee6c5a707c50120bfa2c1411b422366b248e24b
Manish Gupta <manish@dbsentry.com>
2020-08-26 20:43:15 -0500

    20200826 - License headers

--- src/main.js
+++ src/main.js
@@ -1,3 +1,15 @@
+/****************************************************************************
+*                       Confidentiality Information                         *
+*                                                                           *
+* This module is the confidential and proprietary information of            *
+* DBSentry Corp.; it is not to be copied, reproduced, or transmitted in any *
+* form, by any means, in whole or in part, nor is it to be used for any     *
+* purpose other than that for which it is expressly provided without the    *
+* written permission of DBSentry Corp.                                      *
+*                                                                           *
+* Copyright (c) 2020-2021 DBSentry Corp.  All Rights Reserved.              *
+*                                                                           *
+****************************************************************************/
 // =========================================================
 // * Vue Material Dashboard - v1.4.0
 // =========================================================
@@ -37,7 +49,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 
 const options = {
   isEnabled: true,
-  logLevel: isProduction ? 'debug' : 'debug',
+  logLevel: isProduction ? 'error' : 'debug',
   stringifyArguments: false,
   showLogLevel: true,
   showMethodName: true,
